b866e9e923fef83a3c3af67f4cbefb15c6f71bb4
sumitra1 <sumitrakumarishaw1@gmail.com>
2023-04-09 21:33:05 +0530

    created food app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+/**
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - RestaurantContainer
+ *    - RestaurantCard
+ *      -Img
+ *      -Name of Res, Star Rating, cuisine
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ */
+
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://static.vecteezy.com/system/resources/previews/007/890/977/original/burger-logo-for-fast-food-business-free-vector.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
+};
+
+const RestaurantCard=()=>{
+  return(
+    <div className="res-card">
+      <h3>Meghna Foods</h3>
+    </div>
+  )
+}
+
+const Body=()=>{
+  return(
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard/>
+      </div>
+    </div>
+  )
+}
+function App() {
+  return <div>
+    <Header/>
+    <Body/>
+  </div>;
 }
 
 export default App;
