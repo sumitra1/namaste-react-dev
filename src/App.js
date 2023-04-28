0d74d431a0a00b5c578c33b6bc3e0518dcb74806
sumitra1 <sumitrakumarishaw1@gmail.com>
2023-04-28 23:55:26 +0530

    removed comments

--- src/App.js
+++ src/App.js
@@ -4,22 +4,6 @@ import Body from './components/Body'
 import About from "./components/About";
 import Contact from "./components/Contact";
 import { Outlet } from "react-router-dom";
-/**
- * Header
- * - Logo
- * - Nav Items
- * Body
- * - Search
- * - RestaurantContainer
- *    - RestaurantCard
- *      -Img
- *      -Name of Res, Star Rating, cuisine
- * Footer
- * - Copyright
- * - Links
- * - Address
- * - Contact
- */
 
 function App() {
   return (
